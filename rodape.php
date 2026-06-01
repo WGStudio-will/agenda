341b9e96b59d83d6641ee4481dae5a200489a9ad
WGStudio-will <willysonnenhohl@gmail.com>
2026-06-01 15:52:01 -0300

    Add files via upload
    fazendo o agenda dia 01/06/2026

---
+++ rodape.php
@@ -0,0 +1,8 @@
+</div>
+        <script type="text/javascript"
+        src="./resource/lib/jquery/js/jquery-4.0.0.js"></script>
+        <script type="text/javascript"
+        src="./resources/lib/bootstrap/js/bootstrap;js"></script>
+        <script type="text/javascript" src="./resource/common/js/crud.js"></script>
+    </body>
+</html>
